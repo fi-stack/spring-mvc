b1f0f245c9f28d95512cdf71b619d3a94e6fa13f
nci-busdev <busdev4543@gmail.com>
2021-10-19 10:52:00 +0700

    handling form submission

--- src/main/java/com/example/mvc/controllers/HomeController.java
+++ src/main/java/com/example/mvc/controllers/HomeController.java
@@ -1,11 +1,13 @@
 package com.example.mvc.controllers;
 
+import com.example.mvc.entity.Product;
 import com.example.mvc.service.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,6 +22,18 @@ public class HomeController {
         model.addAttribute("title", title);
         model.addAttribute("products", productService.findAll());
         return "index";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model) {
+        model.addAttribute("product", new Product());
+        return "add";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product, Model model) {
+        productService.addProduct(product);
+        return "redirect:/";
     }
 
 }
